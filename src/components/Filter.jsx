d5faec57c25e2eb36cfef99d3ce8eae7cf5e0d48
4002-Nonye <royaltysungbin@gmail.com>
2023-06-23 14:22:36 -0700

    update READNE.md

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -8,7 +8,7 @@ const Filter = () => {
   return (
     <div className="flex w-full bg-darkTheme-veryDarkDesaturatedBlue p-4 mt-4 ">
       <div className="flex items-center justify-center w-full gap-4 text-darkTheme-veryDarkGrayishBluedk font-[700] text-md ">
-        <a className="hov" >All</a>
+        <a className="hov active" >All</a>
         <a className="hov" >Active</a>
         <a className="hov">Completed</a>
       </div>
